c206e6526d47a5925373d542a0bdc12f34a51423
Martin Stefanik <stefanik.mar@gmail.com>
2021-12-12 21:02:11 +0100

    Add a distributions module
    The current purpose of the module is to provide distributions for jump
    sizes of a process a la compound Poisson process.

---
+++ processes/distributions.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Probability distributions."""
+
+
+from abc import ABC, abstractmethod
+from typing import Sequence, Union
+
+import numpy as np
+
+from processes.utils import validate_positive_number
+
+
+class Distribution(ABC):
+    """Abstract base class for a probability distributions."""
+
+    @abstractmethod
+    def sample(self, size: Sequence[int]) -> Union[np.ndarray, float]:
+        """Generate a sample from the probability distribution."""
+        raise NotImplementedError("'sample' method not implemented.")
+
+
+class NormalDistribution(Distribution):
+    """Normal probability distribution."""
+
+    def __init__(self, mu: float = 0, sigma: float = 1) -> None:
+        """Initialize a normal distribution."""
+        self.mu = mu
+        self.sigma = sigma
+
+    @property
+    def sigma(self) -> float:
+        """Sigma getter."""
+        return self._sigma
+
+    @sigma.setter
+    def sigma(self, sigma: float) -> None:
+        """Sigma setter."""
+        validate_positive_number(sigma, "sigma")
+        self._sigma = sigma
+
+    def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
+        """Generate a sample from the normal distribution."""
+        return np.random.normal(self.mu, self.sigma, size)
+
+
+class LogNormalDistribution(NormalDistribution):
+    """Log-normal probability distribution."""
+
+    def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
+        """Generate a sample from the probability distribution."""
+        return np.random.lognormal(self.mu, self.sigma, size)
