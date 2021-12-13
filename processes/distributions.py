f75b6dba93d740f4009c6fdf29c6de22caef1e7f
Martin Stefanik <stefanik.mar@gmail.com>
2021-12-13 14:11:19 +0100

    Add some more distributions

--- processes/distributions.py
+++ processes/distributions.py
@@ -45,5 +45,75 @@ class LogNormalDistribution(NormalDistribution):
     """Log-normal distribution."""
 
     def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
-        """Generate a sample from the probability distribution."""
+        """Generate a sample from the log-normal distribution."""
         return np.random.lognormal(self.mu, self.sigma, size)
+
+
+class ExponentialDistribution(Distribution):
+    """Exponential distribution."""
+
+    def __init__(self, lam: float = 1) -> None:
+        self.lam = lam
+
+    @property
+    def lam(self) -> float:  # noqa: D102
+        return self._lam
+
+    @lam.setter
+    def lam(self, value: float) -> None:  # noqa: D102
+        validate_positive_number(value, "mu")
+        self._lam = value
+
+    def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
+        """Generate a sample from the exponential distribution."""
+        return np.random.exponential(self.lam, size)
+
+
+class ChisquaredDistribution(Distribution):
+    """Chi-squared distribution."""
+
+    def __init__(self, nu: float = 1) -> None:
+        self.nu = nu
+
+    @property
+    def nu(self) -> float:  # noqa: D102
+        return self._nu
+
+    @nu.setter
+    def nu(self, value: float) -> None:  # noqa: D102
+        validate_positive_number(value, "nu")
+        self._nu = value
+
+    def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
+        """Generate a sample from the chi-squared distribution."""
+        return np.random.chisquare(self.nu, size)
+
+
+class GammaDistribution(Distribution):
+    """Gamma distribution."""
+
+    def __init__(self, shape: float = 1, scale: float = 1) -> None:
+        self.shape = shape
+        self.scale = scale
+
+    @property
+    def shape(self) -> float:  # noqa: D102
+        return self._shape
+
+    @shape.setter
+    def shape(self, value: float) -> None:  # noqa: D102
+        validate_positive_number(value, "shape")
+        self._shape = value
+
+    @property
+    def scale(self) -> float:  # noqa: D102
+        return self._scale
+
+    @scale.setter
+    def scale(self, value: float) -> None:  # noqa: D102
+        validate_positive_number(value, "scale")
+        self._scale = value
+
+    def sample(self, size: Sequence[int] = 1) -> Union[np.ndarray, float]:
+        """Generate a sample from the gamma distribution."""
+        return np.random.gamma(self.shape, self.scale, size)
